733192e5aace9ecc41157abbe03001101d6222fb
“Datguyovrder” <rhui@hawk.iit.edu>
2018-08-27 21:28:37 -0500

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# system "say hi mom, look no console"
+contact = Contact.new(
+                        first_name: "Bob",
+                        last_name: "Rooooooarman",
+                        email: "carguy@yahoo.com",
+                        phone_number: "3036780055"
+                     )
+
+contact.save
